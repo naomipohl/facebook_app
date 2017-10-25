e02fe275cb080067d0d751e02e3b4bb55f4db5cc
Jacqueline Askins <askinsj@sas.upenn.edu>
2017-10-25 12:30:09 -0400

    Latest homework updates

--- config/application.rb
+++ config/application.rb
@@ -14,6 +14,7 @@ module FacebookLite
     config.generators do |g|
       g.template_engine nil
       g.test_framework nil
+      g.assets false
       g.helper false
       g.jbuilder false
     end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,13 +1,10 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
+require 'spec_helper'
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 # Prevent database truncation if the environment is production
-abort('The Rails environment is running in production mode!') if Rails.env.production?
-require 'spec_helper'
+abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
-require 'rack_session_access/capybara'
-require 'capybara/dsl'
-require 'capybara/rspec'
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
@@ -51,19 +48,4 @@ RSpec.configure do |config|
   #
   # The different available types are documented in the features, such as in
   # https://relishapp.com/rspec/rspec-rails/docs
-  config.infer_spec_type_from_file_location!
-
-  # Filter lines from Rails gems in backtraces.
-  config.filter_rails_from_backtrace!
-
-  Capybara.app = Rack::Builder.parse_file(File.expand_path('../../config.ru', __FILE__)).first
-
-  config.include Capybara::DSL
-  DatabaseCleaner.strategy = :truncation
-
-  config.after(:each) do
-    DatabaseCleaner.clean
-  end
-  # arbitrary gems may also be filtered via:
-  # config.filter_gems_from_backtrace("gem name")
 end
